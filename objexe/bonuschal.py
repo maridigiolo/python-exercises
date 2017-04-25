af08ecbce0c208c783277d907628055b07d5a1d1
Marina <maridigiolo@gmail.com>
2017-04-25 15:40:00 -0500

    day2week2

--- objexe/bonuschal.py
+++ objexe/bonuschal.py
@@ -5,15 +5,22 @@ class Person:
         self.phone = phone
         self.friends = friends
         self.greet_count = 0
+        self.num_unique_people_greeted = 0
+        self.uniquetracker = []
+
 
     def __str__(self):
-        return '{} contact information: {} | {}'.format(self.name, self.email, self.phone)
+        return 'Person: {} {} {}'.format(self.name, self.email, self.phone)
         # print ('Person: {} {} {}'.format(self.name, self.email, self.phone))
 
 
     def greet(self, other_person):
         print('Hello {}, I am {}!'.format(other_person, self.name))
         self.greet_count += 1
+        if other_person not in self.uniquetracker:
+            self.uniquetracker.append(other_person)
+            self.num_unique_people_greeted += 1
+
 #5,6
     def print_contact_info(self):
         print("{}'s email: {}, {}'s phone number: {}".format(self.name, self.email, self.name, self.phone))
@@ -36,21 +43,24 @@ jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456', [])
 #3
 sonny.greet('Jordan')
 sonny.greet('Jordan')
+sonny.greet('Lili')
+sonny.greet('Mary')
+sonny.greet('Joe')
 
 
 
 #4
-jordan.greet('Sonny')
+# jordan.greet('Sonny')
 
 
 #Ex 2 - ownclass - add method 2
-sonny.print_contact_info()
+# sonny.print_contact_info()
 # #5
 # print('Sonny information is: ' + sonny.email, sonny.phone)
 
 
 #Ex 2 - ownclass - add method 2
-jordan.print_contact_info()
+# jordan.print_contact_info()
 # #6
 # print('Jordan information is: ' + jordan.email, jordan.phone)
 
@@ -62,20 +72,24 @@ jordan.print_contact_info()
 # print(len(sonny.friends))
 
 #Ex2 - ownclass - add a add_friend method
-jordan.add_friends(sonny)
-sonny.add_friends(jordan)
+# jordan.add_friends(sonny)
+# sonny.add_friends(jordan)
 
 #Ex2 - ownclass - add a num_friend method
-jordan.num_friends()
-sonny.num_friends()
+# jordan.num_friends()
+# sonny.num_friends()
 
-print(sonny.greet_count)
-print(jordan.greet_count)
+# print(sonny.greet_count)
+# print(jordan.greet_count)
 
-print(jordan)
+# print(jordan.num_unique_people_greeted)
+print(sonny.num_unique_people_greeted)
+
+
+# print(jordan)
 #If you want a print in other way, you can use this:
 # jordan.__str__()
 #but, remeber, to use that you should print instead of return when you def you str function.
 
-print(sonny)
+# print(sonny)
 # sonny.__str__()
